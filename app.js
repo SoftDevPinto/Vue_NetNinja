e99a1f9fd4377c6d1ab93a423b590024049d40aa
Chris Pinto <Chris.pinto1234@gmail.com>
2023-06-23 12:57:20 -0230

    mouseover

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>' 
     data() {
         return {
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            showBooks: true,
+            url: 'http://www.thenetninja.co.uk',
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss'},
+                { title: 'the way of kings', author: 'brandon sanderson'},
+                { title: 'the final empire', author: 'brandon sanderson'},
+            ],
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         changeTitle(title) {
             // this.title = 'Words of Radiance'
             this.title = title
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks // toggle the value true or false
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 
